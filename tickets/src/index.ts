2c7482a768d55d134409ebfc2b81969199c4ca33
Sajal <karkeesajal@gmail.com>
2021-04-02 22:15:27 -0400

    added messages

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
 
 const start = async () => {
+    console.log('starting....')
+
 
     if(!process.env.JWT_KEY){
         throw new Error('Webtoken is required')
