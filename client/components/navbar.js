33d8057a76fffba2eeb69f4da3945724d022a13c
Sajal <karkeesajal@gmail.com>
2021-04-04 19:19:20 -0400

    bug fix

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,7 +4,7 @@ export default ({currentUser}) =>{
     const links = [
         !currentUser && {label: 'Sign Up', href:'/auth/signup'},
         !currentUser && {label: 'Sign In', href:'/auth/signin'},
-        currentUder && {label: 'Sell Tickets', href:'/tickets/new'},
+        currentUser && {label: 'Sell Tickets', href:'/tickets/new'},
         currentUser && {label: 'My Orders', href:'/orders'},
         currentUser && {label: 'Sign Out', href:'/auth/signout'}
     ]
